a1bfb73b269bff2bf6ee48503c2b1ed4fc47bcb7
QiLin Xue <645367@pdsb.net>
2018-12-15 04:31:03 -0500

    manually loaded data (the api sucked)

--- public/loadData.js
+++ public/loadData.js
@@ -1,12 +1,13 @@
+// Name - Value - Profit - Next Approach - Period - Minerals
 var asteroid_list = [
-                        ["162173 Ryugu",35,865]
-                        ["1989 ML",4,0.6],
-                        ["4660 Nereus",1,0.23],
-                        ["65803 Didymos",22,0.8],
-                        ["2011 UW158",2,0.311],
-                        ["1943 Anteros",1250,2.43],
-                        ["2001 CC21",30,2.561],
-                        ["1992 TC",17,1.085],
-                        ["2001 SG10",0.6,2.43],
-                        ["2002 DO3",0.06,2.11]
+                        ["Ryugu",82.76,30.08,2020,4,"nickel, iron, cobalt, water, nitrogen, hydrogen, ammonia"]
+                        ["1989 ML",13.94,4.38,2019,3,"nickel, iron, cobalt"],
+                        ["Nereus",4.71,1.39,2021,10,"nickel, iron, cobalt"],
+                        ["Bennu",0.670,0.185,2023,2,"iron, hydrogen, ammonia, nitrogen"],
+                        ["Didymos",62.25,16.41,2022,20,"nickel, iron, cobalt"],
+                        ["2011 UW158",6.69,1.74,2046,22,"platinum, nickel, iron, cobalt"],
+                        ["Anteros",5570,1250,2038,12,"magnesium silicate, aluminum, iron silicate"],
+                        ["2001 CC21",147,29.8,2022,15,"magnesium silicate, aluminum, iron silicate"],
+                        ["1992 TC",84.01,16.78,2037,40,"nickel, iron, cobalt"],
+                        ["2001 SG10",3.05,0.54448,2022,6,"nickel, iron, cobalt"],
                     ]
