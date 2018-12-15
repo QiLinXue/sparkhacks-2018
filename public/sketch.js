57e85fa3207da36994adb6e243fb28c2fe6d6d8a
44Four44 <ethanwang422@gmail.com>
2018-12-14 22:53:57 -0500

    asteroid positioning

--- public/sketch.js
+++ public/sketch.js
@@ -1,20 +1,33 @@
 // Global variables
 var mode = 0;
 var asteroids = [];
-
+var positions = [[]];
 
 function setup(){
-    createCanvas(window.innerWidth,window.innerHeight);
+    createCanvas(window.innerWidth, window.innerHeight);
+
+    // Create asteroids
     for (var i = 0; i < 10; i++) {
         asteroids.push(new Asteroid(random(10, 100), random(1000, 10000), random(1000000, 100000000)));
     }
+
+    // Create positions of asteroids
+    for (var i = 0; i < asteroids.length; i++) {
+        positions.push([(width - 80)/asteroids.length * (i+1) + 40, height/2]);
+    }
+    
     print(asteroids);
+    print(positions);
 }
 
 function draw(){
     background(50);
-    for (var i = 0; i < asteroids.length; i ++) {
+    for (var i = 0; i < asteroids.length; i++) {
         asteroid = asteroids[i];
-        asteroid.display(40 + 60 * i, height/2, 50);
+        asteroid.display(positions[i][0], positions[i][1], 50);
     }
+}
+
+function mousePressed() {
+    print("click")
 }
