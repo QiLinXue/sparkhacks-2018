963914510112bd7f4788ddf1eb990492b4575b5d
44Four44 <ethanwang422@gmail.com>
2018-12-15 00:01:09 -0500

    sidebar

--- public/sketch.js
+++ public/sketch.js
@@ -3,6 +3,10 @@ var mode = 0;
 var asteroids = [];
 var positions = [[]];
 
+// Interface
+var sideExpanded = false;
+var sideClickClosed = false;
+
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
 
@@ -22,12 +26,57 @@ function setup(){
 
 function draw(){
     background(50);
+
+    // Draw asteroids
     for (var i = 0; i < asteroids.length; i++) {
         asteroid = asteroids[i];
         asteroid.display(positions[i][0], positions[i][1], 50);
     }
+
+    // Title banner
+    fill(20,48,49);
+    noStroke();
+    rect(0, 0, width, 60);
+
+    textAlign(CENTER, CENTER);
+    textSize(60);
+    fill(255);
+    text("A X I", width/2, 35);
+
+    // Side bar on the right
+    stroke(255);
+    strokeWeight(3);
+
+    line(width - 60, 15, width - 20, 15);
+    line(width - 60, 30, width - 20, 30);
+    line(width - 60, 45, width - 20, 45);
+
+    if (!sideExpanded) {
+        if (mouseX >= width-65 && mouseY <= 60) {
+            if (!sideClickClosed) {
+                sideExpanded = true;
+            }
+        } else {
+            sideClickClosed = false;
+        }
+    } else {
+        strokeWeight();
+        stroke(20, 48, 49);
+        fill(255);
+        rect(width - 300, 60, 300, height-60);
+
+        if (mouseX < width - 300 ) {
+            sideExpanded = false;
+        }
+    }
 }
 
-function mousePressed() {
-    print("click")
-}
+// function mousePressed() {
+//     // Side bar
+//     if (mouseX >= width-65 && mouseY <= 60) {
+//         if (sideExpanded) {
+//         sideClickClosed = true;
+//         }
+//     sideExpanded = !sideExpanded
+//     }
+// }
